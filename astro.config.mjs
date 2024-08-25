96f6d63ca7c41520e3c71f78ee83b49e27a3c1fe
David Wang <56206093+davidlwg@users.noreply.github.com>
2024-08-25 01:06:14 -0400

    add www

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://davidwang.dev",
+  site: "https://www.davidwang.dev",
 });
